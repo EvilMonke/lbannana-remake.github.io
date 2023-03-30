7df86b97f3718652a2b52fad25cf49f695943fae
lbananna <100804904+lbannana@users.noreply.github.com>
2023-03-30 16:07:59 -0400

    Create games.js

---
+++ static/data/games.js
@@ -0,0 +1,10 @@
+var games = [
+  {
+    'id': '1',
+    'title': '1',
+    'url': 'https://rawcdn.githack.com/lioxryt/lioxryt-assets/fe1b89681ac9a807a331b3a51014c47be3ca0269/other/1/index.html',
+    'image': 'https://raw.githubusercontent.com/lioxryt/lioxryt-assets/main/other/1/favicon.ico'
+  },
+  ]
+
+console.log("Loaded " + games.length + " games")
